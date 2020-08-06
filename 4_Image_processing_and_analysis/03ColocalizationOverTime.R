71ff76f4917717ebdd8306dde836ed3d1bbf89e4
Kota Miura <kotapub@gmail.com>
2020-08-06 14:55:50 +0200

    translated R script comments to japanses

--- 4_Image_processing_and_analysis/03ColocalizationOverTime.R
+++ 4_Image_processing_and_analysis/03ColocalizationOverTime.R
@@ -1,22 +1,22 @@
-# requires EBImage
+# EBImageのパッケージを使う
 library(EBImage)
-# EBImage can be installed using
+# EBImage は次の2つのコマンドでインストールできる（インストール済みの場合は不要）。
 install.packages("BiocManager")
 BiocManager::install("EBImage")
-# import each channel using EBImage
+# EBImageを使って、各チャネルをインポート
 c1 <- readImage("c1.tif")
 c2 <- readImage("c2.tif")
-# assign the number of frames to a variable
+# フレーム数を変数として取得
 fr = dim(c1)[3]
-# make two empty vectors to receive SRCC results
+# SRCCの結果を格納する空のベクトルをふたつ用意する
 r1 <- rep(NA, fr)
 r2 <- rep(NA, fr)
-# use a for loop to get SRCC for each frame
+# 各フレームにおけるSRCCをループを回して計算
 for (i in 1:fr ){
   r1[i] <- cor(as.vector(c1[,,i]), as.vector(c2[,,i]), method="spearman")
   r2[i] <- cor(sample(as.vector(c1[,,i])), as.vector(c2[,,i]), method="spearman")
 }
-# plot out the result using base R
+# Rのベース関数を使ってプロット
 png("corr.png")
 par(cex = 1.5, mar = c(4, 4, 1, 1))
 plot(0,0, type="l", xlim=c(0,fr), ylim=c(-0.1,1.2), xlab="Frame", ylab="SRCC")
